f3e3ea9a9f887db65c5e497e47ba8e2bb7fb5e71
darl4real <darl4real@hotmail.com>
2018-10-11 11:21:24 +0200

    project initiatted

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,55 @@
+// built-in
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+// components
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+//routes
+import { appRoutes } from './routes';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { UserService } from './shared/user.service';
+//other
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    SignInComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent,
+
+  ],
+  imports: [
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+  ],
+
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  },AuthGuard,UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    //public authService: AuthService,
+    private router: Router,
+    //private flashMessage: FlashMessagesService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogoutClick() {
+    //this.authService.logout();
+    //this.flashMessage.show('You are now logged out', {cssClass: 'alert-success', timeout: 2000});
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
